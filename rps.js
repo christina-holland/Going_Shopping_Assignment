f2423756680cd90b9f5bb4fd7d3ab0465ce804d1
christina holland <cholland369@gmail.com>
2024-06-25 15:27:08 -0400

    added comments to files

--- rps.js
+++ rps.js
@@ -1,14 +1,17 @@
+//Setting variables that will access the buttons and score counts in the HTML
 const rockButton = document.getElementById("rock");
-const paperButton = document.querySelector("#paper");
-const scissorsButton = document.querySelector("#scissors");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const tieText = document.getElementById("result-tie");
 
+//Defining variables for the amount of wins
 let computerWinAmount = 0;
 let playerWinAmount = 0;
 let tieAmount = 0;
 
+//Setting the event listeners to assign an outcome for the player's hand when a button is selected and then play a round of the game
 rockButton.addEventListener("click", function(){
     let hand1 = "rock";
     let hand2 = computerPlay();
@@ -27,6 +30,7 @@ scissorsButton.addEventListener("click", function() {
     playRound(hand1, hand2);
 });
 
+//Setting the computerPlay function to randomly make a choice for the computer's hand
 function computerPlay() {
     let choice = parseInt(Math.random()*10)%3;
     switch(choice) {
@@ -43,6 +47,7 @@ function computerPlay() {
     return choice;
 }
 
+//Setting functions to increase the score totals based on who wins the game
 function playerWin() {
     playerWinAmount++;
     playerScore.innerHTML = "Your wins: " + playerWinAmount;
@@ -58,6 +63,7 @@ function tieWin() {
     tieText.innerHTML = "Tie rounds: " + tieAmount;
 }
 
+//Setting a function called playRound that determines the winner of the game
 function playRound(hand1, hand2) {
     if ( (hand1 === 'scissors' && hand2 === 'paper') ||
          (hand1 === 'rock' && hand2 === 'scissors') ||
@@ -73,10 +79,18 @@ function playRound(hand1, hand2) {
         computerWin();
         }
 
-    else {
+    else if ( (hand1 === 'scissors' && hand2 === 'scissors') ||
+              (hand1 === 'rock' && hand2 === 'rock') ||
+              (hand1 === 'paper' && hand2 === 'paper') )
+        {
         tieWin(); 
         }
+    
+    else {
+        return null;
+    }
 }
 
+//Calling the function playRound
 playRound();
 
